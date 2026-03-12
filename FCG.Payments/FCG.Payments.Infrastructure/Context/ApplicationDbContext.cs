e1eba59a832c5798f3d494c41fbf01edb9228a01
Miguel084 <134954882+Miguel084@users.noreply.github.com>
2026-03-12 00:41:49 -0300

    Refactor ApplicationDbContext to use options pattern
    Removed multiple constructors and connection string handling from ApplicationDbContext.

--- FCG.Payments/FCG.Payments.Infrastructure/Context/ApplicationDbContext.cs
+++ FCG.Payments/FCG.Payments.Infrastructure/Context/ApplicationDbContext.cs
@@ -9,22 +9,8 @@ namespace FCG.Payments.Infrastructure.Context
 {
     public class ApplicationDbContext : DbContext
     {
-        private readonly string _connectionString;
-
-        public ApplicationDbContext()
-        {
-            IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            _connectionString = configuration.GetConnectionString("ConnectionStrings");
-        }
-
-        public ApplicationDbContext(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
 
         #region DbSet
 
@@ -33,17 +19,11 @@ namespace FCG.Payments.Infrastructure.Context
 
         #endregion
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(_connectionString, builder => builder.EnableRetryOnFailure());
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
